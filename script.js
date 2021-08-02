30968f62d3d53c66a31ab6864a880af3254140a9
NazarulxFitri <63062783+NazarulxFitri@users.noreply.github.com>
2021-08-02 16:11:45 +0800

    snow rotating animation

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-setInterval(createSnowFlake, 100);
+setInterval(createSnowFlake, 200);
 
 function createSnowFlake() {
     const snow_flake = document.createElement('i');
     snow_flake.classList.add('fas');
     snow_flake.classList.add('fa-snowflake');
-    snow_flake.style.left = Math.random() * window.innerWidth - 50 + 'px';
+    snow_flake.style.left = Math.random() * window.innerWidth - 25 + 'px';
     snow_flake.style.animationDuration = Math.random() * 5 + 5 + 's';
     snow_flake.style.opacity = Math.random();
     snow_flake.style.fontSize = Math.random() * 10 + 10 + 'px';
